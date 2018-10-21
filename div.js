d104b6d9a6800c8f698493a231e8636a6b3763e5
eleni <helenig12@gmail.com>
2018-10-21 00:40:44 -0500

    side bar half works still needs to be pretty

--- div.js
+++ div.js
@@ -7,7 +7,7 @@ var div = document.createElement("div");
 div.class = "sidenav";
 div.id = "putTheShitHere";
 div.style.float = "right";
-div.style.background = "#4286f4";
+div.style.background =  "#423d49";
 
 var p = document.createElement("p");
 p.id = "instructions";
@@ -15,14 +15,19 @@ p.innerHTML = "characters";
 
 div.appendChild(p);
 
+var divslide = document.createElement("div");
+divslide.id= "slideout";
+divslide.class = "sidenav";
 
 
 var bub = document.createElement("button");
 bub.id = "nightmodebub"; // aria-hidden
 
+div.appendChild(bub);
+document.getElementsByTagName("body")[0].appendChild(divslide);
+document.getElementById("slideout").appendChild(div);
 
-document.getElementsByTagName("body")[0].appendChild(div);
-document.getElementsByTagName("div")[0].appendChild(bub);
+
 
 
 document.getElementById('nightmodebub').addEventListener('click', function() {
